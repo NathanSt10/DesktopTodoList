b3cb9f02d135f7bd906988daa4c94c2423a07dd3
Nathan Stout <NathanielStout@Eaton.com>
2025-06-04 14:09:45 -0500

    Added Tasks model, view, viewmodel

---
+++ TodoList/Model/TodoTask.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoList.Model
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool? Reoccuring { get; set; }
+        public DayOfWeek? DayOfWeek { get; set; }
+    }
+}
---
+++ TodoList/ViewModel/TasksViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TodoList.Model;
+
+namespace TodoList.ViewModel
+{
+    public class TasksViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ObservableCollection<TodoTask> Tasks { get; private set; } = new();
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public virtual Task LoadAsync() => Task.CompletedTask;
+    }
+}
